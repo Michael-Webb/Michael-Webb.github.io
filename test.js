ed44976e5075dad4b9401d5a63beb0e3ce4ef19b
Michael-Webb <64173608+Michael-Webb@users.noreply.github.com>
2023-11-20 06:56:44 -0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,39 @@
+define(["bi/glass/common/ui/ProgressToast"], function (n) {
+  "use strict";
+
+  var CognosModule = (function () {
+
+
+    CognosModule.prototype.initialize = function initialize() {
+      var _this = this;
+
+      var mapContent = this.canvas.getContent(STORE_MAP_CONTENT_ID);
+      var state = mapContent.getFeature("state");
+      state.whenStatusChanges(state.STATUS.RENDERED).then(function () {
+        window.parent.postMessage("dashboard.ready", "*");
+        __getDashboardAPI();
+      });
+
+      this._createProgressToast = function () {
+        return new n();
+      };
+
+      try {
+        this.progressBar = this._createProgressToast();
+        this.progressBar.show({
+          type: "file",
+          id: "idUploadUIElement",
+          name: "uploadfiles",
+          style: "display: none;",
+          multiple: !1,
+        });
+        this.progressBar.progress();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return CognosModule;
+  })();
+
+});
