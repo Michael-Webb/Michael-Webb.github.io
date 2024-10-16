f774bc1f06c8104ad889c60a817080a4eee42b67
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2024-10-16 15:01:52 -0700

    css5

--- SelectwithSearch.js
+++ SelectwithSearch.js
@@ -237,41 +237,42 @@ define(function () {
 
     sHtml += `
 			  <button class="myBtn">
-				  <div id="main-container">
-					  <div id="container-header">
-             <div id="search-input-container">
-						  <input id="myInput" type="text" placeholder="Search.." autocomplete="off"/>
-						    <span id="search-icon" >
-							    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-								    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-							    </svg>
-						    </span>
+          <div id="main-container">
+            <div id="container-header">
+              <div id="search-container">
+                <div id="search-input-container">
+                  <input id="myInput" type="text" placeholder="Search.." autocomplete="off"/>
+                  <span id="search-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                    </svg>
+                  </span>
+                </div>
+                ${!oConfig.AutoSubmit ? '<span id="applyFilter-btn">Apply</span>' : ""}
               </div>
-						  ${!oConfig.AutoSubmit ? '<span id="applyFilter-btn">Apply</span>' : ""}
-
-					  </div>
-					  <div id="container-body" ${
+              <select id="filter-options">
+                <option value="startAny">Starts with any of these Keywords</option>
+                <option value="containAny">Contains any of these Keywords</option>
+                <option value="start">Starts With The Keyword</option>
+                <option value="contain">Contains the Keyword</option>  
+              </select>
+            </div>
+            <div id="container-body" ${
               iRowCount > iScrollIfMoreThan ? 'style="overflow-y:auto; height:' + iScrollIfMoreThan * 32 + 'px"' : ""
             }>
-						  ${checkboxValues}
-					  </div> 
-					  <hr>
-					  <div id="container-footer">
-						  <select id="filter-options">
-                <option value="startAny">Starts with any of these Keywords</option>
-    						<option value="containAny">Contains any of these Keywords</option>
-							  <option value="start">Starts With The Keyword</option>
-    						<option value="contain">Contains the Keyword</option>  
-						  </select>
-						  <span id="clearFilter-btn">Deselect All</span>
+              ${checkboxValues}
+            </div>
+            <hr>
+            <div id="container-footer">
+              <span id="clearFilter-btn">Deselect All</span>
               <span id="selectAll-btn">Select All</span>
-					  </div>
-				  </div>
-				  <span>${sLabel}</span>
-				  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-					  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-				  </svg>
-			  </button>
+            </div>
+          </div>
+          <span>${sLabel}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        </button>
 		  `;
 
     el.innerHTML = sHtml;
