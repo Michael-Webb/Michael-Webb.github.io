a3587bfbf8ca5d66060e120c0ee6ac0c65063c8e
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-02-06 09:13:14 -0800

    asdf

--- Cognos/Select and Search/DropdownSearch/js/SingleSelectTest.js
+++ Cognos/Select and Search/DropdownSearch/js/SingleSelectTest.js
@@ -112,7 +112,7 @@ define(() => {
         }
         sHtml += `</select>`;
         oControlHost.container.innerHTML = sHtml;
-        this.m_sel = oControlHost.container.getElementById(selectId);
+        this.m_sel = oControlHost.container.querySelector(`#${selectId}`);
 
         if (this.mainParamValues && this.mainParamValues.length > 0) {
           this.m_sel.value = this.mainParamValues[0];
@@ -425,4 +425,4 @@ define(() => {
 
   return CustomControl;
 });
-/* 908 */
+/* 913 */
