50fe6272c0729e39c82df90d64549b106eca693d
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-02-06 16:21:30 -0800

    pppp

--- Cognos/Select and Search/DropdownSearch/js/refactorMultiSelect.js
+++ Cognos/Select and Search/DropdownSearch/js/refactorMultiSelect.js
@@ -741,8 +741,8 @@ define(() => {
       this.iconSvg = this.oControlHost.container.querySelector("#iconSvg");
 
       // Define the SVG paths
-      const magnifyingGlassSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>`;
-      const clearSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`;
+      const magnifyingGlassSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>`;
+      const clearSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`;
 
       // Function to update the search icon based on the input value
       const updateSearchIcon = () => {
