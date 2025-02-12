f666c6ac3b3f297b9f12384f3cae454f0a3acf1e
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-02-12 08:44:48 -0800

    asdf

--- Cognos/Select and Search/DropdownSearch/js/refactorMultiSelect.js
+++ Cognos/Select and Search/DropdownSearch/js/refactorMultiSelect.js
@@ -738,7 +738,7 @@ define(() => {
 
       // New: Get references to the search icon button and the SVG container.
       this.searchIcon = this.dropdown.querySelector("#searchIcon");
-      this.iconSvg = this.searchIcon.querySelector("#iconSvg");
+      this.iconSvg = this.searchIcon.querySelector("svg");
 
       // Define the SVG paths
       const magnifyingGlassSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#808080" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>`;
