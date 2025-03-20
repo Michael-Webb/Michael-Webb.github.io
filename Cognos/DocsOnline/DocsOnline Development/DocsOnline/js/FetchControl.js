20b6d14b8a5305ecdc8053ac4d900a73218ecd49
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-03-20 12:56:53 -0700

    added updates for debugging

--- Cognos/DocsOnline/DocsOnline Development/DocsOnline/js/FetchControl.js
+++ Cognos/DocsOnline/DocsOnline Development/DocsOnline/js/FetchControl.js
@@ -358,7 +358,7 @@ define([], function () {
 
         this.showLoadingIcon(span);
 
-        // Build the URL. 
+        // Build the URL. test2
         
         const cleanArg = DocumentsOnline.url_Decode(arg).replace(/\\/g, "");
         console.log("Arg",arg)
