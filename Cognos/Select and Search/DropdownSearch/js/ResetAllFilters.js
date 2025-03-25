2218e73e9cdd5f435eb2f39aa2547aec0736f360
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-03-24 17:07:39 -0700

    asdf

--- Cognos/Select and Search/DropdownSearch/js/ResetAllFilters.js
+++ Cognos/Select and Search/DropdownSearch/js/ResetAllFilters.js
@@ -3,6 +3,8 @@ define(function () {
 
   class ResetAllParameters {
     draw(oControlHost) {
+      let controlNames = oControlHost.configuration.ControlNames;
+      let controlNamesArray = controlNames.split(",").map((item) => item.trim());
       const el = oControlHost.container;
       el.innerHTML =
         "<style>" +
@@ -11,11 +13,16 @@ define(function () {
         "</style>" +
         '<button class="myButton btnClear" type="button">Clear</button>';
 
-      el.querySelector(".btnClear").onclick = this.f_clearButtonClick.bind(this, oControlHost);
+      el.querySelector(".btnClear").onclick = this.f_clearButtonClick.bind(this, oControlHost, controlNamesArray);
     }
-    f_clearButtonClick(oControlHost) {
-      const aControls = oControlHost.page.getControlsByName(true);
-      console.log("aControls",aControls)
+
+    f_clearButtonClick(oControlHost, controlArray) {
+      // Loop through each control name in the array
+      controlArray.forEach((controlName) => {
+        const aControls = oControlHost.page.getControlsByName(controlName);
+        console.log(`Controls for "${controlName}":`, aControls);
+      });
+
       oControlHost.page.application.clearParameterValues();
       oControlHost.finish();
     }
