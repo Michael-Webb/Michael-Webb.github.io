01f904f65d319da79c126d907ba93e5dec697703
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-03-26 16:46:52 -0700

    asdf

--- Cognos/Select and Search/DropdownSearch/js/ReactTest.js
+++ Cognos/Select and Search/DropdownSearch/js/ReactTest.js
@@ -1,48 +1,43 @@
-define([], function() {
-    "use strict";
-  
-    class AdvancedControl {
-      initialize(oControlHost, fnDoneInitializing) {
-        require([
-          "https://unpkg.com/react@18/umd/react.production.min",
-          "https://unpkg.com/react-dom@18/umd/react-dom.production.min"
-        ], this.dependenciesLoaded.bind(this, fnDoneInitializing));
-      }
-  
-      dependenciesLoaded(fnDoneInitializing, React, ReactDOM) {
-        // Store the loaded dependencies
-        this.React = React;
-        this.ReactDOM = ReactDOM;
-  
-        // Define an inline React component that renders some text
-        this.MyReactComponent = function(props) {
-          return React.createElement("div", null, props.text);
-        };
-  
-        fnDoneInitializing();
-      }
-  
-      // Render the inline React component with a text prop
-      draw(oControlHost) {
-        this.ReactDOM.render(
-          this.React.createElement(this.MyReactComponent, { text: "Hello from React!" }),
-          oControlHost.container
-        );
-      }
-  
-      // Unmount the component when the control is destroyed
-      destroy(oControlHost) {
-        this.ReactDOM.unmountComponentAtNode(oControlHost.container);
-      }
-  
-      // Other lifecycle methods as needed
-      show(oControlHost) {}
-      hide(oControlHost) {}
-      isInValidState(oControlHost) {}
-      getParameters(oControlHost) {}
-      setData(oControlHost, oDataStore) {}
+define([], function () {
+  "use strict";
+
+  class AdvancedControl {
+    initialize(oControlHost, fnDoneInitializing) {
+      require([
+        "https://unpkg.com/react@18/umd/react.production.min",
+        "https://unpkg.com/react-dom@18/umd/react-dom.production.min",
+      ], this.dependenciesLoaded.bind(this, fnDoneInitializing));
     }
-  
-    return AdvancedControl;
-  });
-  
+
+    dependenciesLoaded(fnDoneInitializing, React, ReactDOM) {
+      // Store the loaded dependencies
+      this.React = React;
+      this.ReactDOM = ReactDOM;
+
+      fnDoneInitializing();
+    }
+
+    // Render the inline React component with a text prop
+    draw(oControlHost) {
+      const MyReactComponent = (props) => this.React.createElement("div", null, props.text);
+      this.ReactDOM.render(
+        this.React.createElement(MyReactComponent, { text: "Hello from React!" }),
+        oControlHost.container
+      );
+    }
+
+    // Unmount the component when the control is destroyed
+    destroy(oControlHost) {
+      this.ReactDOM.unmountComponentAtNode(oControlHost.container);
+    }
+
+    // Other lifecycle methods as needed
+    show(oControlHost) {}
+    hide(oControlHost) {}
+    isInValidState(oControlHost) {}
+    getParameters(oControlHost) {}
+    setData(oControlHost, oDataStore) {}
+  }
+
+  return AdvancedControl;
+});
