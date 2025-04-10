fa9d0922277c7aaeb939a1dc0171b230ab4f2fbf
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-04-10 09:27:49 -0700

    asdsadf

--- Cognos/DocsOnline/DocsOnline Development/DocsOnline/js/FE_CC.js
+++ Cognos/DocsOnline/DocsOnline Development/DocsOnline/js/FE_CC.js
@@ -311,27 +311,27 @@ define(() => {
           credentials: "omit",
         });
 
-        const response2 = await fetch(screenUrl, {
-            headers: {
-              "accept": "*/*",
-              "accept-language": "en-US,en;q=0.9",
-              "cache-control": "no-cache",
-              "pragma": "no-cache",
-              "priority": "u=1, i",
-              "sec-fetch-dest": "document",
-              "sec-fetch-mode": "navigate",
-              "sec-fetch-site": "same-site",
-              "sec-fetch-user": "?1",
-              "upgrade-insecure-requests": "1",
-            },
-            referrer: this.JobUrl,
-            referrerPolicy: "strict-origin-when-cross-origin",
-            body: null,
-            method: "GET",
-            mode: "no-cors",
-            credentials: "include",
-          });
-        return response2;
+        // const response2 = await fetch(screenUrl, {
+        //     headers: {
+        //       "accept": "*/*",
+        //       "accept-language": "en-US,en;q=0.9",
+        //       "cache-control": "no-cache",
+        //       "pragma": "no-cache",
+        //       "priority": "u=1, i",
+        //       "sec-fetch-dest": "document",
+        //       "sec-fetch-mode": "navigate",
+        //       "sec-fetch-site": "same-site",
+        //       "sec-fetch-user": "?1",
+        //       "upgrade-insecure-requests": "1",
+        //     },
+        //     referrer: this.JobUrl,
+        //     referrerPolicy: "strict-origin-when-cross-origin",
+        //     body: null,
+        //     method: "GET",
+        //     mode: "no-cors",
+        //     credentials: "include",
+        //   });
+        return response;
       } catch (error) {
         console.error("Error during FAUPAS fetch:", error);
         throw error;
@@ -341,4 +341,4 @@ define(() => {
 
   return AdvancedControl;
 });
-// 20250410 923
+// 20250410 927
