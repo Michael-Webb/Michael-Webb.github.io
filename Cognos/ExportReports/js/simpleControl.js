f491a475828df0be92d4511e7fe73f6ae9de3939
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-04-13 17:21:33 -0700

    asdf

---
+++ Cognos/ExportReports/js/simpleControl.js
@@ -0,0 +1,15 @@
+define(() => {
+  "use strict";
+  class AdvancedControl {
+    initialize(oControlHost, fnDoneInitializing) {
+      fnDoneInitializing();
+    }
+
+    draw(oControlHost) {
+      let spanNodeList = document.querySelectorAll('span[data-id^="cc_"]');
+      console.log(spanNodeList)
+    }
+  }
+
+  return AdvancedControl;
+});
