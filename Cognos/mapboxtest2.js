5b885a3ea91f890f3ca5e8d4153a8171ffd47ee4
Michael Webb <64173608+Michael-Webb@users.noreply.github.com>
2025-04-17 10:29:33 -0700

    asdf

--- Cognos/mapboxtest2.js
+++ Cognos/mapboxtest2.js
@@ -171,9 +171,8 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.js", "jquery
 .marker {
   border: none;
   cursor: pointer;
-  height: 56px;
-  width: 56px;
-  background-image: url('marker.png');
+  height: 24px;
+  width: 24px;
 }
   .mapboxgl-popup {
   padding-bottom: 50px;
@@ -419,6 +418,18 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.js", "jquery
         el.id = `marker-${feature.properties.id}`;
         el.className = "marker";
 
+        // 2️⃣ inject the SVG directly
+        el.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" 
+           width="24" height="24" viewBox="0 0 24 24" 
+           fill="none" stroke="currentColor" stroke-width="2" 
+           stroke-linecap="round" stroke-linejoin="round" 
+           class="lucide lucide-map-pin-icon lucide-map-pin">
+        <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/>
+        <circle cx="12" cy="10" r="3"/>
+      </svg>
+    `;
+
         el.addEventListener("click", (evt) => {
           // fly & popup
           this.flyToStore(feature);
@@ -440,4 +451,4 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v3.3.0/mapbox-gl.js", "jquery
 
   return BasicControl;
 });
-//v16
+//v17
